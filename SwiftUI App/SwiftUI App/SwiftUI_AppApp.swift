fa2e859c0de5020c021e52c1d4ff595ee3bd51a6
Aynur19 <nasybullin.aynur@yandex.ru>
2023-03-11 15:28:23 +0300

    created project; added tab view with tip calculator;

---
+++ SwiftUI App/SwiftUI App/SwiftUI_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_AppApp.swift
+//  SwiftUI App
+//
+//  Created by Aynur Nasybullin on 09.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
