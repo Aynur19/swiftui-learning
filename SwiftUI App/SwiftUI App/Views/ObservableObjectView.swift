29e5c9405ab5ae230d2e8ae5d1addbf670e7e802
Aynur19 <nasybullin.aynur@yandex.ru>
2023-03-13 19:25:48 +0300

    added "Observable Object" view;

---
+++ SwiftUI App/SwiftUI App/Views/ObservableObjectView.swift
@@ -0,0 +1,37 @@
+//
+//  ObservableObjectView.swift
+//  SwiftUI App
+//
+//  Created by Aynur Nasybullin on 13.03.2023.
+//
+
+import SwiftUI
+
+class User: ObservableObject {
+    @Published var firstName = "FirstName"
+    @Published var lastName = "LastName"
+}
+
+struct ObservableObjectView: View {
+    @ObservedObject var user = User()
+    
+    var body: some View {
+        VStack(alignment: .center) {
+            Text("Hello \(user.firstName) \(user.lastName)!")
+            
+            TextField("First name", text: $user.firstName)
+                .padding(.horizontal, 24)
+                .padding(.vertical, 12)
+            
+            TextField("Last name", text: $user.lastName)
+                .padding(.horizontal, 24)
+                .padding(.vertical, 12)
+        }
+    }
+}
+
+struct ObservableObjectView_Previews: PreviewProvider {
+    static var previews: some View {
+        ObservableObjectView()
+    }
+}
