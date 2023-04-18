cf23b3f8301b9161ab0d232a20ba6b07100334d8
Aynur19 <nasybullin.aynur@yandex.ru>
2023-04-18 21:29:53 +0300

    added big emojis pack

---
+++ Memorize/Memorize/Models/MemorizeGameData.swift
@@ -0,0 +1,256 @@
+//
+//  MemorizeGameData.swift
+//  Memorize
+//
+//  Created by Aynur Nasybullin on 17.04.2023.
+//
+
+import Foundation
+
+
+
+class MemorizeGameData {
+    public static func getSmileyFaceEmojis() -> [String] {
+        ["🙂", "😀", "😃", "😄", "😁", "😅", "😆", "🤣", "😂", "🙃", "😉", "😊", "😇", "😎", "🤓", "🧐", "🥳"]
+    }
+    
+    public static func getSmileyFaceEmojis() -> [String] {
+        ["Emotional Faces Emojis", "EMOJI", "🥰", "😍", "🤩", "😘", "😗", "😚", "😙", "🥲"]
+    }
+    
+    public static func getSmileyFaceEmojis() -> [String] {
+        ["Faces with Tongue Emojis", "EMOJI", "😋", "😛", "😜", "🤪", "😝", "🤑"]
+    }
+    
+    public static func getSmileyFaceEmojis() -> [String] {
+        ["Faces with Hands Emojis", "EMOJI", "🤗", "🤭", "🤫", "🤔"]
+    }
+    
+    public static func getSmileyFaceEmojis() -> [String] {
+        ["Neutral Faces Emojis", "EMOJI", "😐", "🤐", "🤨", "😑", "😶", "😏", "😒", "🙄", "😬", "🤥"]
+    }
+    
+    public static func getSmileyFaceEmojis() -> [String] {
+        ["Sleepy Faces Emojis", "EMOJI", "😪", "😴", "😌", "😔", "🤤"]
+    }
+    
+    public static func getSmileyFaceEmojis() -> [String] {
+        ["Sick Faces Emojis", "EMOJI", "😷", "🤒", "🤕", "🤢", "🤮", "🤧", "🥵", "🥶", "🥴", "😵", "🤯"]
+    }
+    
+    public static func getSmileyFaceEmojis() -> [String] {
+        ["Concerned Faces Emojis", "EMOJI", "😕", "😟", "🙁", "☹", "😮", "😯", "😲", "😳", "🥺", "😦", "😧", "😨", "😰", "😥", "😢", "😭", "😱", "😖", "😣", "😞", "😓", "😩", "😫", "🥱"]
+    }
+    
+    public static func getSmileyFaceEmojis() -> [String] {
+        ["Negative Faces Emojis", "EMOJI", "😤", "😡", "😠", "🤬", "😈", "👿", "💀"]
+    }
+    
+    public static func getSmileyFaceEmojis() -> [String] {
+        ["Costume Faces Emojis", "EMOJI", "💩", "🤡", "👹", "👺", "👻", "👽", "👾", "🤖"]
+    }
+    
+    public static func getSmileyFaceEmojis() -> [String] {
+        ["Faces that require more than one unicode character", "EMOJI", "😶‍🌫️", "😮‍💨", "😵‍💫", "❤️‍🔥", "❤️‍🩹", "👁️‍🗨️", "🧔‍♂️", "🧔‍♀️", "👨‍🦰", "👨‍🦱", "👨‍🦳", "👨‍🦲", "👩‍🦰", "🧑‍🦰", "👩‍🦱", "🧑‍🦱", "👩‍🦳", "🧑‍🦳", "👩‍🦲", "🧑‍🦲", "👱‍♀️", "👱‍♂️"]
+    }
+    
+    public static func getSmileyFaceEmojis() -> [String] {
+        ["Cat Faces Emojis", "EMOJI", "😺", "😸", "😹", "😻", "😼", "😽", "🙀", "😿", "😾"]
+    }
+    
+    public static func getSmileyFaceEmojis() -> [String] {
+        ["Monkey Faces Emojis", "EMOJI", "🙈", "🙉", "🙊"]
+    }
+    
+    public static func getSmileyFaceEmojis() -> [String] {
+        ["Emotion Emojis", "EMOJI", "💋", "💌", "💘", "💝", "💖", "💗", "💓", "💞", "💕", "💟", "❣", "💔", "❤️‍🔥", "❤️‍🩹", "❤", "🧡", "💛", "💚", "💙", "💜", "🤎", "🖤", "🤍", "💯", "💢", "💥", "💫", "💦", "💨", "🕳", "💣", "💬", "👁️‍🗨️", "🗨", "🗯", "💭", "💤"]
+    }
+    
+    public static func getSmileyFaceEmojis() -> [String] {
+        ["Hands and other Body Parts Emojis", "EMOJI", "👋", "🤚", "🖐", "✋", "🖖", "👌", "🤌", "🤏", "✌", "🤞", "🤟", "🤘", "🤙", "👈", "👉", "👆", "👇", "🖕", "☝", "🫵", "👍", "👎", "✊", "👊", "🤛", "🤜", "👏", "🙌", "👐", "🤲", "🤝", "🙏", "✍", "💅", "🤳", "💪", "🦾", "🦵", "🦿", "🦶", "👂", "🦻", "👃", "🧠", "👣", "🫀", "🫁", "🦷", "🦴", "👀", "👁", "👅", "👄"]
+    }
+    
+    public static func getSmileyFaceEmojis() -> [String] {
+        ["Person Emojis", "EMOJI", "🧑", "👶", "🧒", "👦", "👧", "👱", "👨", "🧔", "🧔‍♂‍", "🧔‍♀‍", "👨‍🦰", "👨‍🦱", "👨‍🦳", "👨‍🦲", "👩", "👩‍🦰", "👩‍🦱", "👩‍🦳", "👩‍🦲", "👱‍♀‍", "👱‍♂‍", "🧓", "👴", "👵", "🙍", "🙍‍♂️", "🙍‍♀️", "🙎‍♂️", "🙎‍♀️", "🙅‍♂️", "🙅‍♀️", "🙆‍♂️", "🙆‍♀️", "💁‍♂️", "💁‍♀️", "🙋‍♂️", "🙋‍♀️", "🧏‍♂️", "🧏‍♀️", "🙇‍♂️", "🙇‍♀️", "🤦‍♂️", "🤦‍♀️", "🤷‍♂️", "🤷‍♀️", "🧑‍⚕️", "👨‍⚕️", "👩‍⚕️", "🧑‍🎓", "👨‍🎓", "👩‍🎓", "🧑‍🏫", "👨‍🏫", "👩‍🏫", "🧑‍⚖️", "👩‍⚖️", "🧑‍🌾", "👨‍🌾", "👩‍🌾", "🧑‍🍳", "👨‍🍳", "👩‍🍳", "🧑‍🔧", "👨‍🔧", "👩‍🔧", "🧑‍🏭", "👨‍🏭", "👩‍🏭", "🧑‍💼", "👨‍💼", "👩‍💼", "🧑‍🔬", "👨‍🔬", "👩‍🔬", "🧑‍💻", "👨‍💻", "👩‍💻", "🧑‍🎤", "👨‍🎤", "👩‍🎤", "🧑‍🎨", "👨‍🎨", "👩‍🎨", "🧑‍✈️", "👨‍✈️", "👩‍✈️", "🧑‍🚀", "👨‍🚀", "👩‍🚀", "🧑‍🚒", "👨‍🚒", "👩‍🚒", "👮‍♂️", "👮‍♀️", "🕵️‍♂️", "🕵️‍♀️", "💂‍♂️", "💂‍♀️", "👷‍♂️", "👷‍♀️", "👳‍♂️", "👳‍♀️", "🤵‍♂️", "🤵‍♀️", "👰‍♂️", "👰‍♀️", "🧑‍🍼", "👩‍🍼", "👨‍🍼", "🧑‍🎄", "🦸‍♂️", "🦸‍♀️", "🦹‍♂️", "🦹‍♀️", "🧙‍♂️", "🧙‍♀️", "🧚‍♂️", "🧚‍♀️", "🧛‍♂️", "🧛‍♀️", "🧜‍♂️", "🧜‍♀️", "🧝‍♂️", "🧝‍♀️", "🧞‍♂️", "🧞‍♀️", "🧟‍♂️", "🧟‍♀️", "💆‍♂️", "💆‍♀️", "💇‍♂️", "💇‍♀️", "🚶‍♂️", "🚶‍♀️", "🧍‍♂️", "🧍‍♀️", "🧎‍♂️", "🧎‍♀️", "🧑‍🦯", "👨‍🦯", "👩‍🦯", "🧑‍🦼", "👨‍🦼", "👩‍🦼", "🧑‍🦽", "👨‍🦽", "👩‍🦽", "🏃‍♂️", "🏃‍♀️", "👯‍♂️", "👯‍♀️", "🧖‍♂️", "🧖‍♀️", "🧗‍♂️", "🧗‍♀️", "🏌️‍♂️", "🏌️‍♀️", "🧗", "🧗‍♂‍", "🧗‍♀‍", "🤺", "🏇", "⛷", "🏂", "🏌", "🏌️‍♂‍", "🏌️‍♀‍", "🏄", "🏄‍♂️", "🏄‍♀️", "🚣", "🚣‍♂️", "🚣‍♀️", "🏊", "🏊‍♂️", "🏊‍♀️", "⛹", "⛹️‍♂️", "⛹️‍♀️", "🏋", "🏋️‍♂️", "🏋️‍♀️", "🚴", "🚴‍♂️", "🚴‍♀️", "🚵", "🚵‍♂️", "🚵‍♀️", "🤸", "🤸‍♂️", "🤸‍♀️", "🤼", "🤼‍♂️", "🤼‍♀️", "🤽", "🤽‍♂️", "🤽‍♀️", "🤾", "🤾‍♂️", "🤾‍♀️", "🤹", "🤹‍♂️", "🤹‍♀️", "🧘", "🧘‍♂️", "🧘‍♀️", "🛀", "🛌"]
+    }
+    
+    public static func getSmileyFaceEmojis() -> [String] {
+        ["Family Emojis", "EMOJI", "👪", "👨‍👩‍👦", "👨‍👩‍👧", "👨‍👩‍👧‍👦", "👨‍👩‍👦‍👦", "👨‍👩‍👧‍👧", "👨‍👨‍👦", "👨‍👨‍👧", "👨‍👨‍👧‍👦", "👨‍👨‍👦‍👦", "👨‍👨‍👧‍👧", "👩‍👩‍👦", "👩‍👩‍👧", "👩‍👩‍👧‍👦", "👩‍👩‍👦‍👦", "👩‍👩‍👧‍👧", "👨‍👦", "👨‍👦‍👦", "👨‍👧", "👨‍👧‍👦", "👨‍👧‍👧", "👩‍👦", "👩‍👦‍👦", "👩‍👧", "👩‍👧‍👦", "👩‍👧‍👧", "🧑‍🤝‍🧑", "👭", "👫", "👬", "💏", "👩‍❤️‍💋‍👨", "👨‍❤️‍💋‍👨", "👩‍❤️‍💋‍👩", "👩‍❤️‍👨", "👨‍❤️‍👨", "👩‍❤️‍👩", "💑", "🗣", "👤", "👥", "🫂"]
+    }
+    
+    public static func getSmileyFaceEmojis() -> [String] {
+        ["Animals and Nature Emojis", "EMOJI", "🐵", "🐒", "🦍", "🦧", "🐶", "🐕", "🦮", "🐕‍🦺", "🐩", "🐺", "🦊", "🦝", "🐱", "🐈", "🐈‍⬛", "🦁", "🐯", "🐅", "🐆", "🐴", "🐎", "🦄", "🦓", "🦌", "🦬", "🐮", "🐄", "🐂", "🐃", "🐷", "🐖", "🐗", "🐽", "🐏", "🐑", "🐐", "🐪", "🐫", "🦙", "🦒", "🐘", "🦣", "🦏", "🦛", "🐭", "🐁", "🐀", "🐹", "🐰", "🐇", "🐿", "🦫", "🦔", "🦇", "🐻", "🐻‍❄️", "🐨", "🐼", "🦥", "🦦", "🦨", "🦘", "🦡", "🐾", "🦃", "🐔", "🐓", "🐣", "🐤", "🐥", "🐦", "🐦‍⬛", "🐧", "🕊", "🦅", "🦆", "🦢", "🦉", "🦤", "🪶", "🦩", "🦜", "🐸", "🐊", "🐢", "🦎", "🐍", "🐲", "🐉", "🦕", "🦖", "🐳", "🐋", "🐬", "🦭", "🐟", "🐠", "🐡", "🦈", "🐙", "🐚", "🐌", "🦋", "🐛", "🐜", "🐝", "🪲", "🐞", "🦗", "🪳", "🕷", "🕸", "🦂", "🦟", "🪰", "🪱", "🦠", "💐", "🌸", "💮", "🏵", "🌹", "🥀", "🌺", "🌻", "🌼", "🌷", "🌱", "🪴", "🌲", "🌳", "🌴", "🌵", "🌾", "🌿", "☘", "🍀", "🍁", "🍂", "🍃", "🪹", "🪺"]
+    }
+    
+    public static func getSmileyFaceEmojis() -> [String] {
+        ["Food and Drinks Emojis", "EMOJI", "🍇", "🍈", "🍉", "🍊", "🍋", "🍌", "🍍", "🥭", "🍎", "🍏", "🍐", "🍑", "🍒", "🍓", "🫐", "🥝", "🍅", "🫒", "🥥", "🥑", "🍆", "🥔", "🥕", "🌽", "🌶", "🫑", "🥒", "🥬", "🥦", "🧄", "🧅", "🍄", "🥜", "🫑", "🌰", "🍞", "🥐", "🥖", "🫓", "🥨", "🥯", "🥞", "🧇", "🧀", "🍖", "🍗", "🥩", "🥓", "🍔", "🍟", "🍕", "🌭", "🥪", "🌮", "🌯", "🫔", "🥙", "🧆", "🥚", "🍳", "🥘", "🍲", "🫕", "🥣", "🥗", "🍿", "🧈", "🧂", "🥫", "🍱", "🍘", "🍙", "🍚", "🍛", "🍜", "🍝", "🍠", "🍢", "🍣", "🍤", "🍥", "🥮", "🍡", "🥟", "🥠", "🥡", "🦀", "🦞", "🦐", "🦑", "🦪", "🍨", "🍧", "🍦", "🍩", "🍪", "🎂", "🍰", "🧁", "🥧", "🍫", "🍬", "🍭", "🍮", "🍯", "🍼", "🥛", "☕", "🫖", "🍵", "🍶", "🍾", "🍷", "🍸", "🍹", "🍺", "🍻", "🥂", "🥃", "🥤", "🧋", "🧃", "🧉", "🧊", "🥢", "🍽", "🍴", "🥄", "🔪", "🧋", "🏺"]
+    }
+    
+    public static func getSmileyFaceEmojis() -> [String] {
+        ["Travel and Places Emojis", "EMOJI", "🌍", "🌎", "🌏", "🌐", "🗺", "🧭", "⛰", "🏔", "🌋", "🗻", "🏕", "🏖", "🏜", "🏝", "🏞", "🏟", "🏛", "🏗", "🧱", "🪨", "🪵", "🛖", "🏘", "🏚", "🏠", "🏡", "🏢", "🏣", "🏤", "🏥", "🏦", "🏨", "🏩", "🏪", "🏫", "🏬", "🏭", "🏯", "🏰", "💒", "🗼", "🗽", "⛪", "🕌", "🛕", "🕍", "⛩", "🕋", "⛲", "⛺", "🌁", "🌃", "🏙", "🌅", "🌄", "🌆", "🌇", "🌉", "♨", "🎠", "🎡", "🎢", "💈", "🎪"]
+    }
+    
+    public static func getSmileyFaceEmojis() -> [String] {
+        ["Transport Emojis", "EMOJI", "🚂", "🚃", "🚄", "🚅", "🚆", "🚇", "🚈", "🚉", "🚊", "🚝", "🚞", "🚋", "🚌", "🚍", "🚎", "🚐", "🚑", "🚒", "🚓", "🚔", "🚕", "🚖", "🚗", "🚘", "🚙", "🛻", "🚚", "🚛", "🚜", "🏎", "🏍", "🛵", "🦽", "🦼"]
+    }
+    
+    public static func getSmileyFaceEmojis() -> [String] {
+        ["Time Emojis", "EMOJI", "⌛", "⏳", "⌚", "⏰", "⏱", "⏲", "🕰", "🕛", "🕧", "🕐", "🕜", "🕑", "🕝", "🕒", "🕞", "🕓", "🕟", "🕔", "🕠", "🕕", "🕡", "🕖", "🕢", "🕗", "🕣", "🕘", "🕤", "🕙", "🕥", "🕚", "🕦"]
+    }
+    
+    public static func getSmileyFaceEmojis() -> [String] {
+        ["Sky and Weather Emojis", "EMOJI", "🌑", "🌒", "🌓", "🌔", "🌕", "🌖", "🌗", "🌘", "🌙", "🌚", "🌛", "🌜", "🌡", "☀", "🌝", "🌞", "🪐", "⭐", "🌟", "🌠", "🌌", "☁", "⛅", "⛈", "🌤", "🌥", "🌦", "🌧", "🌨", "🌩", "🌪", "🌫", "🌬", "🌀", "🌈", "🌂", "☂", "☔", "⛱", "⚡", "❄", "☃", "⛄", "☄", "🔥", "💧", "🌊"]
+    }
+    
+    public static func getSmileyFaceEmojis() -> [String] {
+        ["Activity Emojis", "EMOJI", "🎃", "🎄", "🎆", "🎇", "🧨", "✨", "🎈", "🎉", "🎊", "🎋", "🎍", "🎎", "🎏", "🎑", "🧧", "🎀", "🎁", "🎗", "🎟", "🎫"]
+    }
+    
+    public static func getSmileyFaceEmojis() -> [String] {
+        ["Award Medals Emojis", "EMOJI", "🎖", "🏆", "🏅", "🥇", "🥈", "🥉"]
+    }
+    
+    public static func getSmileyFaceEmojis() -> [String] {
+        ["Sport Emojis", "EMOJI", "⚽", "⚾", "🥎", "🏀", "🏐", "🏈", "🏉", "🎾", "🥏", "🎳", "🏏", "🏑", "🏒", "🥍", "🏓", "🏸", "🥊", "🥋", "🥅", "⛳", "⛸", "🎣", "🤿", "🎽", "🎿", "🛷", "🥌"]
+    }
+    
+    public static func getSmileyFaceEmojis() -> [String] {
+        ["Games Emojis", "EMOJI", "🎯", "🪀", "🪁", "🎱", "🔮", "🪄", "🧿", "🪬", "🎮", "🕹", "🎰", "🎲", "🧩", "🧸", "🪅", "🪆", "♠", "♥", "♣", "♟", "🃏", "🀄", "🎴"]
+    }
+    
+    public static func getSmileyFaceEmojis() -> [String] {
+        ["Arts and Crafts Emojis", "EMOJI", "🎭", "🖼", "🎨", "🧵", "🪡", "🧶", "🪢"]
+    }
+    
+    public static func getSmileyFaceEmojis() -> [String] {
+        ["Clothing Objects Emojis", "EMOJI", "👓", "🕶", "🥽", "🥼", "🦺", "👔", "👕", "👖", "🧣", "🧤", "🧥", "🧦", "👗", "👘", "🥻", "🩱", "🩲", "🩳", "👙", "👚", "👛", "👜", "👝", "🛍", "🎒", "🩴", "👞", "👟", "🥾", "🥿", "👠", "👡", "🩰", "👢", "👑", "👒", "🎩", "🎓", "🧢", "🪖", "⛑", "📿", "💄", "💍", "💎"]
+    }
+    
+    public static func getSmileyFaceEmojis() -> [String] {
+        ["Sound Emojis", "EMOJI", "🔇", "🔈", "🔉", "🔊", "📢", "📣", "📯", "🔔", "🔕", "🎼", "🎵", "🎶", "🎙", "🎚", "🎛", "🎤", "🎧", "📻"].shuffled()
+    }
+    
+    public static func getSmileyFaceEmojis() -> [String] {
+        ["Musical Instrument Emojis", "EMOJI", "🎷", "🪗", "🎸", "🎹", "🎺", "🎻", "🪕", "🥁", "🪘"].shuffled()
+    }
+    
+    public static func getSmileyFaceEmojis() -> [String] {
+        ["Phone Emojis", "EMOJI", "📱", "📲", "☎", "📞", "📟", "📠"].shuffled()
+    }
+    
+    public static func getSmileyFaceEmojis() -> [String] {
+        ["Computer Emojis", "EMOJI", "🔋", "🪫", "🔌", "💻", "🖥", "🖨", "⌨", "🖱", "🖲", "💽", "💾", "💿", "📀", "🧮"].shuffled()
+    }
+    
+    public static func getSmileyFaceEmojis() -> [String] {
+        ["Light and Video Emojis", "EMOJI", "🎥", "🎞", "📽", "🎬", "📺", "📷", "📸", "📹", "📼", "🔍", "🔎", "🕯", "💡", "🔦", "🏮", "🪔"].shuffled()
+    }
+    
+    public static func getSmileyFaceEmojis() -> [String] {
+        ["Book and Paper Emojis", "EMOJI", "📔", "📕", "📖", "📗", "📘", "📙", "📚", "📓", "📒", "📃", "📜", "📄", "📰", "🗞", "📑", "🔖", "🏷", "💰", "🪙", "💴", "💵", "💶", "💷", "💸", "💳", "🧾", "💹"].shuffled()
+    }
+    
+    public static func getSmileyFaceEmojis() -> [String] {
+        ["Mail Emojis", "EMOJI", "✉", "📧", "📩", "📤", "📥", "📦", "📫", "📪", "📬", "📭", "📮", "🗳"].shuffled()
+    }
+    
+    public static func getSmileyFaceEmojis() -> [String] {
+        ["Writing Emojis", "EMOJI", "✏", "✒", "🖋", "🖊", "🖌", "🖍", "📝"].shuffled()
+    }
+    
+    public static func getSmileyFaceEmojis() -> [String] {
+        ["Office Emojis", "EMOJI", "💼", "📁", "📂", "🗂", "📅", "📆", "📇", "📈", "📉", "📊", "📋", "📌", "📍", "📎", "🖇", "📏", "📐", "✂", "🗃", "🗄", "🗑"].shuffled()
+    }
+    
+    public static func getSmileyFaceEmojis() -> [String] {
+        ["Lock Emojis", "EMOJI", "🔒", "🔓", "🔏", "🔐", "🔑", "🗝"].shuffled()
+    }
+    
+    public static func getSmileyFaceEmojis() -> [String] {
+        ["Tools Emojis", "EMOJI", "🔨", "🪓", "⛏", "⚒", "🛠", "🗡", "⚔", "🔫", "🪃", "🏹", "🛡", "🪚", "🔧", "🪛", "🔩", "⚙", "🗜", "⚖", "🦯", "🔗", "⛓", "🪝", "🧰", "🧲", "🪜"].shuffled()
+    }
+    
+    public static func getSmileyFaceEmojis() -> [String] {
+        ["Science Emojis", "EMOJI", "⚗", "🧪", "🧫", "🧬", "🔬", "🔭", "📡"].shuffled()
+    }
+    
+    public static func getSmileyFaceEmojis() -> [String] {
+        ["Medical Emojis", "EMOJI", "💉", "🩸", "💊", "🩹", "🩼", "🩺", "🩻"].shuffled()
+    }
+    
+    public static func getSmileyFaceEmojis() -> [String] {
+        ["Household Emojis", "EMOJI", "🚪", "🛗", "🪞", "🪟", "🛏", "🛋", "🪑", "🚽", "🪠", "🚿", "🛁", "🪤", "🪒", "🧴", "🧷", "🧹", "🧺", "🧻", "🪣", "🧼", "🫧", "🪥", "🧽", "🧯", "🛒"].shuffled()
+    }
+    
+    public static func getSmileyFaceEmojis() -> [String] {
+        ["Other Objects Emojis", "EMOJI", "🚬", "⚰", "🪦", "⚱", "🗿", "🪧", "🪪"].shuffled()
+    }
+    
+    public static func getSmileyFaceEmojis() -> [String] {
+        ["Symbols", "EMOJI", "🏧", "🚮", "🚰", "♿", "🚹", "🚺", "🚻", "🚼", "🚾", "🛂", "🛃", "🛄", "🛅"].shuffled()
+    }
+    
+    public static func getSmileyFaceEmojis() -> [String] {
+        ["Warning Emojis", "EMOJI", "⚠", "🚸", "⛔", "🚫", "🚳", "🚭", "🚯", "🚱", "🚷", "📵", "🔞", "☢", "☣"].shuffled()
+    }
+    
+    public static func getSmileyFaceEmojis() -> [String] {
+        ["Arrow Emojis", "EMOJI", "⬆", "↗", "➡", "↘", "⬇", "↙", "⬅", "↖", "↕", "↔", "↩", "↪", "⤴", "⤵", "🔃", "🔄", "🔙", "🔚", "🔛", "🔜", "🔝"].shuffled()
+    }
+    
+    public static func getSmileyFaceEmojis() -> [String] {
+        ["Religion", "EMOJI", "🛐", "⚛", "🕉", "✡", "☸", "☯", "✝", "☦", "☪", "☮", "🕎", "🔯"].shuffled()
+    }
+    
+    public static func getSmileyFaceEmojis() -> [String] {
+        ["Zodiac", "EMOJI", "♈", "♉", "♊", "♋", "♌", "♍", "♎", "♏", "♐", "♑", "♒", "♓", "⛎"].shuffled()
+    }
+    
+    public static func getSmileyFaceEmojis() -> [String] {
+        ["AV Symbols", "EMOJI", "🔀", "🔁", "🔂", "▶", "⏸", "⏩", "⏭", "⏯", "◀", "⏪", "⏮", "🔼", "⏫", "🔽", "⏬", "⏹", "⏺", "⏏", "🎦", "🔅", "🔆", "📶", "📳", "📴"].shuffled()
+    }
+    
+    public static func getSmileyFaceEmojis() -> [String] {
+        ["Gender", "EMOJI", "♀", "♂", "⚧"].shuffled()
+    }
+    
+    public static func getSmileyFaceEmojis() -> [String] {
+        ["Math Symbols", "EMOJI", "✖", "➕", "➖", "➗", "🟰", "♾"].shuffled()
+    }
+    
+    public static func getSmileyFaceEmojis() -> [String] {
+        ["Punctuation Symbols", "EMOJI", "‼", "⁉", "❓", "❔", "❗", "❕", "〰"].shuffled()
+    }
+    
+    public static func getSmileyFaceEmojis() -> [String] {
+        ["Currency", "EMOJI", "💱", "💲"].shuffled()
+    }
+    
+    public static func getSmileyFaceEmojis() -> [String] {
+        ["Other Symbols", "EMOJI", "⚕", "♻", "⚜", "🔱", "📛", "🔰", "⭕", "✅", "☑", "✔", "❌", "❎", "➰", "➿", "〽", "✳", "✴", "❇", "©", "®", "™"].shuffled()
+    }
+    
+    public static func getSmileyFaceEmojis() -> [String] {
+        ["Keycap", "EMOJI", "#️⃣", "*️⃣", "0️⃣", "1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️⃣", "7️⃣", "8️⃣", "9️⃣", "🔟"].shuffled()
+    }
+    
+    public static func getSmileyFaceEmojis() -> [String] {
+        ["Alphanumeric Symbols", "EMOJI", "🔠", "🔡", "🔢", "🔣", "🔤", "🅰", "🆎", "🅱", "🅾", "🆑", "🆒", "🆓", "ℹ", "🆔", "Ⓜ", "🆕", "🆖", "🆗", "🅿", "🆘", "🆙", "🆚"].shuffled()
+    }
+    
+    public static func getSmileyFaceEmojis() -> [String] {
+        ["Japanese Buttons", "EMOJI", "🈁", "🈂", "🈷", "🈶", "🈯", "🉐", "🈹", "🈚", "🈲", "🉑", "🈸", "🈴", "🈳", "㊗", "㊙", "🈺", "🈵"].shuffled()
+    }
+    
+    public static func getSmileyFaceEmojis() -> [String] {
+        ["Geometric Emojis", "EMOJI", "🔴", "🟠", "🟡", "🟢", "🔵", "🟣", "🟤", "⚫", "⚪", "🟥", "🟧", "🟨", "🟩", "🟦", "🟪", "🟫", "⬛", "⬜", "🔶", "🔷", "🔸", "🔹", "🔺", "🔻", "💠", "🔘", "🔳", "🔲"].shuffled()
+    }
+    
+    public static func getSmileyFaceEmojis() -> [String] {
+        ["Flags", "EMOJI", "🏁", "🚩", "🎌", "🏴", "🏳", "🏳️‍🌈", "🏳️‍⚧️", "🏴‍☠️"].shuffled()
+    }
+    
+    public static func getSmileyFaceEmojis() -> [String] {
+        ["Country Flags", "EMOJI", "🇦🇨", "🇦🇩", "🇦🇪", "🇦🇫", "🇦🇬", "🇦🇮", "🇦🇱", "🇩🇿", "🇦🇲", "🇦🇴", "🇦🇶", "🇦🇷", "🇦🇸", "🇦🇹", "🇦🇺", "🇦🇼", "🇦🇽", "🇦🇿", "🇧🇦", "🇧🇩", "🇧🇪", "🇧🇫", "🇧🇬", "🇧🇭", "🇧🇮", "🇧🇯", "🇧🇱", "🇧🇲", "🇧🇳", "🇧🇴", "🇧🇶", "🇧🇷", "🇧🇸", "🇧🇹", "🇧🇻", "🇧🇼", "🇧🇾", "🇧🇿", "🇨🇦", "🇨🇨", "🇨🇩", "🇨🇫", "🇨🇬", "🇨🇭", "🇨🇮", "🇨🇰", "🇨🇱", "🇨🇲", "🇨🇳", "🇨🇴", "🇨🇵", "🇨🇷", "🇨🇺", "🇨🇻", "🇨🇼", "🇨🇽", "🇨🇾", "🇨🇿", "🇩🇪", "🇩🇬", "🇩🇯", "🇩🇰", "🇩🇲", "🇩🇴", "🇪🇦", "🇪🇨", "🇪🇪", "🇪🇬", "🇪🇭", "🇪🇷", "🇪🇸", "🇪🇹", "🇪🇺", "🇫🇮", "🇫🇯", "🇫🇰", "🇫🇲", "🇫🇴", "🇫🇷", "🇬🇦", "🇬🇧", "🇬🇩", "🇬🇪", "🇬🇫", "🇬🇬", "🇬🇭", "🇬🇮", "🇬🇱", "🇬🇲", "🇬🇳", "🇬🇵", "🇬🇶", "🇬🇷", "🇬🇸", "🇬🇹", "🇬🇺", "🇬🇼", "🇬🇾", "🇭🇰", "🇭🇲", "🇭🇳", "🇭🇷", "🇭🇹", "🇭🇺", "🇮🇨", "🇮🇩", "🇮🇪", "🇮🇱", "🇮🇲", "🇮🇳", "🇮🇴", "🇮🇶", "🇮🇷", "🇮🇸", "🇮🇹", "🇯🇪", "🇯🇲", "🇯🇴", "🇯🇵", "🇰🇪", "🇰🇬", "🇰🇭", "🇰🇮", "🇰🇲", "🇰🇳", "🇰🇵", "🇰🇷", "🇰🇼", "🇰🇾", "🇰🇿", "🇱🇦", "🇱🇧", "🇱🇨", "🇱🇮", "🇱🇰", "🇱🇷", "🇱🇸", "🇱🇹", "🇱🇺", "🇱🇻", "🇱🇾", "🇲🇦", "🇲🇨", "🇲🇩", "🇲🇪", "🇲🇫", "🇲🇬", "🇲🇭", "🇲🇰", "🇲🇱", "🇲🇲", "🇲🇳", "🇲🇴", "🇲🇵", "🇲🇶", "🇲🇷", "🇲🇸", "🇲🇹", "🇲🇺", "🇲🇻", "🇲🇼", "🇲🇽", "🇲🇾", "🇲🇿", "🇳🇦", "🇳🇨", "🇳🇪", "🇳🇬", "🇳🇫", "🇳🇮", "🇳🇱", "🇳🇴", "🇳🇵", "🇳🇷", "🇳🇺", "🇳🇿", "🇴🇲", "🇵🇦", "🇵🇪", "🇵🇫", "🇵🇬", "🇵🇭", "🇵🇰", "🇵🇱", "🇵🇲", "🇵🇳", "🇵🇷", "🇵🇸", "🇵🇹", "🇵🇼", "🇵🇾", "🇶🇦", "🇷🇪", "🇷🇴", "🇷🇸", "🇷🇺", "🇷🇼", "🇸🇦", "🇸🇧", "🇸🇨", "🇸🇩", "🇸🇪", "🇸🇬", "🇸🇭", "🇸🇮", "🇸🇯", "🇸🇰", "🇸🇱", "🇸🇲", "🇸🇳", "🇸🇴", "🇸🇷", "🇸🇸", "🇸🇹", "🇸🇻", "🇸🇽", "🇸🇾", "🇸🇿", "🇹🇦", "🇹🇨", "🇹🇩", "🇹🇫", "🇹🇬", "🇹🇭", "🇹🇯", "🇹🇰", "🇹🇱", "🇹🇲", "🇹🇳", "🇹🇴", "🇹🇷", "🇹🇹", "🇹🇻", "🇹🇼", "🇹🇿", "🇺🇦", "🇺🇬", "🇺🇲", "🇺🇳", "🇺🇸", "🇺🇾", "🇺🇿", "🇻🇦", "🇻🇨", "🇻🇪", "🇻🇬", "🇻🇮", "🇻🇳", "🇻🇺", "🇼🇫", "🇼🇸", "🇽🇰", "🇾🇪", "🇾🇹", "🇿🇦", "🇿🇲", "🇿🇼", "🏴󠁧󠁢󠁥󠁮󠁧󠁿", "🏴󠁧󠁢󠁳󠁣󠁴󠁿", "🏴󠁧󠁢󠁷󠁬󠁳󠁿"]
+         }
+}
--- Memorize/Memorize/Views/MemorizeView.swift
+++ Memorize/Memorize/Views/MemorizeView.swift
@@ -11,19 +11,24 @@ struct MemorizeView: View {
     @ObservedObject var game: EmojiMemoryGame
     
     var body: some View {
-        ScrollView {
-            LazyVGrid(columns: [GridItem(.adaptive(minimum: 80))]) {
-                ForEach(game.cards) { card in
-                    CardView(card: card)
-                        .aspectRatio(2/3, contentMode: .fit)
-                        .onTapGesture {
-                            game.choose(card)
-                        }
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 80))]) {
+                    ForEach(game.cards) { card in
+                        CardView(card: card)
+                            .aspectRatio(2/3, contentMode: .fit)
+                            .onTapGesture {
+                                game.choose(card)
+                            }
+                    }
                 }
             }
+            .foregroundColor(.red)
+            .padding(.horizontal)
+            .navigationTitle("Memorize")
+            .navigationBarTitleDisplayMode(.inline)
         }
-        .foregroundColor(.red)
-        .padding(.horizontal)
+        
     }
 }
 
