2a2de4cab1561af116486ea7fb1969cee739f442
Aynur19 <nasybullin.aynur@yandex.ru>
2023-05-16 21:54:30 +0300

    added initializers and addEmoji() func;

--- EmojiArt/EmojiArt/Models/EmojiArtModel.swift
+++ EmojiArt/EmojiArt/Models/EmojiArtModel.swift
@@ -8,19 +8,36 @@
 import Foundation
 
 struct EmojiArtModel {
-    var background: Background
+    var background = Background.blank
     var emojis = [Emoji]()
+    private var uniqueEmojiId = 0
     
-    struct Background {
+    init() { }
+    
+    struct Emoji {
         let content: String
         var x: Int
         var y: Int
         var size: Int
+        let id: Int
+        
+        fileprivate init(content: String, x: Int, y: Int, size: Int, id: Int) {
+            self.content = content
+            self.x = x
+            self.y = y
+            self.size = size
+            self.id = id
+        }
     }
     
-    enum Emoji {
+    enum Background {
         case blank
         case url
         case imageData
+    }
+    
+    mutating func addEmoji(_ content: String, at location: (x: Int, y: Int), size: Int) {
+        uniqueEmojiId += 1
+        emojis.append(Emoji(content: content, x: location.x, y: location.y, size: size, id: uniqueEmojiId))
     }
 }
