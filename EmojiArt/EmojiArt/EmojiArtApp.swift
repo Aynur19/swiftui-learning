30e4e84897f288ccc14f766f6b0a3cdfc56a74db
Aynur19 <nasybullin.aynur@yandex.ru>
2023-05-16 21:33:38 +0300

    created project: EmojiArt App;

---
+++ EmojiArt/EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Aynur Nasybullin on 16.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
